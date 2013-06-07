7139c9cb4b085c2901c007f583042bb6e122fbd5
StevenWill <ste.d.williams@gmail.com>
2013-06-07 06:54:24 -0500

    add manifest bash script

---
+++ manifest.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+yum install -y vim
+yum install -y apache2 \
+  mysql-server \
+  php \
+  php-cli \
+  php-mysql \
+  php-mbstring \
+  php-gd \
+  php-pear \
+  php-xml \
+  ruby \
+  git \
+  ruby
+
+gem update system
+gem install compass
+
+service iptables stop
+service httpd start
+chkconfig iptables off
+
+pear channel-discover pear.drush.org
+pear install drush/drush
+
+wget -qO- -O tmp.tgz http://download.pear.php.net/package/Console_Table-1.1.3.tgz && tar -xvzf tmp.tgz
+mv Console_Table-1.1.3 /usr/share/pear/drush/lib/.
+rm tmp.tgz
+
+rm -rf /var/www/html
+ln -fs /vagrant/drupal7 /var/www/html
